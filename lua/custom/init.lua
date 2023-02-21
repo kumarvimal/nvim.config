b2db34f55c392da6f5f4eb7abc0bd6c7916ae993
Kumar Vimal <mail@kumarvimal.in>
2023-02-21 02:38:54 +0100

    Add lsp and other plugins

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,12 @@
 require("custom.set")
 require("custom.remap")
 
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
--- lua/custom/packer.lua
+++ lua/custom/packer.lua
@@ -12,7 +12,66 @@ return require('packer').startup(function(use)
       'nvim-telescope/telescope.nvim', tag = '0.1.0',
       requires = { {'nvim-lua/plenary.nvim'} }
  }
-
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+          require("rose-pine").setup()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
 
   use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+  use("tpope/vim-fugitive")
+
+    use {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v1.x',
+	  requires = {
+		  -- LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  -- Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+		  -- Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+	  }
+  }
+
+  use {
+      "folke/which-key.nvim",
+      config = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+        require("which-key").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+   }
+
+  use {
+      'lewis6991/gitsigns.nvim',
+      config = function()
+        require('gitsigns').setup()
+      end
+    }
+
+  use('ryanoasis/vim-devicons')
+  use {
+      'nvim-lualine/lualine.nvim',
+      requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+
 end)

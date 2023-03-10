936ab4847d1029ae81f6f1203d2e9d43d4ccf8e6
Kumar Vimal <mail@kumarvimal.in>
2023-03-10 23:29:15 +0100

    Update .envrc script

--- scripts/sync_envrc.sh
+++ scripts/sync_envrc.sh
@@ -10,13 +10,6 @@ Ref https://github.com/direnv/direnv
 """
 set -Eeuo pipefail
 
-current_dir=$(pwd -P)
-
-envrc="${current_dir}/envrc"
-
-target_dir=$ENVRC_SYNC_TARGET_DIR
-
-
 envrc="echo \"activating virtualenv\"\n
 git rev-parse --show-toplevel | tee ~/.last_opened_project\n
 
@@ -27,11 +20,16 @@ else\n
   echo \"Virtualenv not found\"\n
 fi
 "
+dirs=$ENCRC_SYNC_TARGET_DIRS
+# e.g /user/home/dev/comp/prefix-*,user/home/dev/dir/*
 
-for d in $target_dir; do
-    echo -e $envrc >  "$d/.envrc"
-    direnv allow "$d"
-    echo  "Synced to $d"
+for target_dir in ${dirs//,/ }
+do
+  for d in $target_dir; do
+      echo -e $envrc >  "$d/.envrc"
+      direnv allow "$d"
+      echo  "Synced to $d"
+  done
 done
 
 echo "done!"

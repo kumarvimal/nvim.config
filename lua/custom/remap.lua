3342fc9df748d3acdc954897de2211218c03a0b7
Kumar Vimal <mail@kumarvimal.in>
2023-03-10 23:39:15 +0100

    Add mapping for :spellgood

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -4,3 +4,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "zg", '<cmd>spellgood<cr>')
+
